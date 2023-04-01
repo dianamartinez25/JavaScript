1b824c559eec701b0f897e0e7afb629ed19e857a
dianamartinez25 <125301816+dianamartinez25@users.noreply.github.com>
2023-04-01 13:39:36 +0200

    Add files via upload

---
+++ cadenas.js
@@ -0,0 +1,18 @@
+//var cadena1 = "Esto es una cadena";
+//var cadena1 = new String("Esto es una cadena");
+//cadena1.length
+
+//Concatenar
+//var cadena1 = "Hola";
+//var cadena2 = "Adiós";
+//cadena1.concat("" + cadena2)
+
+//Mayúsculas, Minúsculas
+
+
+//CharAt (posición) de letra o palabra
+//cadena1.charAt(0)
+
+var palabra = "Hola amigo"
+//document.write('<br>')
+document.write(palabra.substring(2))
